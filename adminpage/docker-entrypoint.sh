05a53ac3226212ea9bb98a78c71223d36a7a878a
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2025-01-29 15:25:05 +0300

    chore: perform preparation commands on container start

--- adminpage/docker-entrypoint.sh
+++ adminpage/docker-entrypoint.sh
@@ -6,7 +6,9 @@ set -e
 . /opt/pysetup/.venv/bin/activate
 
 # You can put other setup logic here
-# ...
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
+python3 manage.py createcachetable
 
 # Evaluating passed command:
 exec "$@"
