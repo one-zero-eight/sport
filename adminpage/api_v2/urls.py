31905c6efa14b96b9178666940084c4333c8bcb5
Aleksandr <billboard163rus@gmail.com>
2025-07-13 17:06:33 +0300

    Renamed the endpoints

--- adminpage/api_v2/urls.py
+++ adminpage/api_v2/urls.py
@@ -33,62 +33,61 @@ register_converter(NegativeIntConverter, 'negint')
 
 urlpatterns = [
     # profile
-    path(r"profile/student", profile.get_student_info),
+    path(r"profile/me", profile.get_student_info),
     path(r"profile/change_gender", profile.change_gender),
     #path(r"profile/history/<int:semester_id>", profile.get_history),
-    path(r"profile/history_with_self/<int:semester_id>", profile.get_history_with_self),
+    path(r"student/history/<int:semester_id>", profile.get_history_with_self),
 
-    # enroll
-    path(r"enrollment/enroll", enroll.enroll),
-    path(r"enrollment/unenroll", enroll.unenroll),
-    path(r"enrollment/unenroll_by_trainer", enroll.unenroll_by_trainer),
+    # enrollment
+    path(r"student/enroll", enroll.enroll),
+    path(r"student/unenroll", enroll.unenroll),
+    path(r"trainer/unenroll-student", enroll.unenroll_by_trainer),
 
-    # group
-    path(r"group/<int:group_id>", group.group_info_view),
+    # groups
+    path(r"groups/<int:group_id>", group.group_info_view),
     path(r"sports", group.sports_view),
 
-    # training
-    path(r"training/<int:training_id>", training.training_info),
-    path(r"training/<int:training_id>/check_in", training.training_checkin),
-    path(r"training/<int:training_id>/cancel_check_in", training.training_cancel_checkin),
+    # trainings
+    path(r"trainings/<int:training_id>", training.training_info),
+    path(r"trainings/<int:training_id>/check-in", training.training_checkin),
+    path(r"trainings/<int:training_id>/cancel-check-in", training.training_cancel_checkin),
 
     # attendance
-    path(r"profile/history/by_date", attendance.get_student_trainings_between_dates),
-    path(r"attendance/suggest_student", attendance.suggest_student),
-    path(r"attendance/<int:training_id>/grades", attendance.get_grades),
-    path(r"attendance/<int:training_id>/grades.csv", attendance.get_grades_csv),
-    path(r"attendance/<int:group_id>/report",
-         attendance.get_last_attended_dates),
+    path(r"student/trainings", attendance.get_student_trainings_between_dates),
+    path(r"attendance/students/search", attendance.suggest_student),
+    path(r"trainings/<int:training_id>/grades", attendance.get_grades),
+    path(r"trainings/<int:training_id>/grades.csv", attendance.get_grades_csv),
+    path(r"groups/<int:group_id>/attendance-report", attendance.get_last_attended_dates),
     path(r"attendance/mark", attendance.mark_attendance),
-    path(r"attendance/<int:student_id>/hours", attendance.get_student_hours_info),
-    path(r"attendance/<int:student_id>/negative_hours", attendance.get_negative_hours_info),
-    path(r"attendance/<int:student_id>/better_than", attendance.get_better_than_info),
+    path(r"students/<int:student_id>/hours", attendance.get_student_hours_info),
+    path(r"students/<int:student_id>/negative-hours", attendance.get_negative_hours_info),
+    path(r"students/<int:student_id>/better-than", attendance.get_better_than_info),
 
     # calendar
-    path(r"calendar/<negint:sport_id>/schedule", calendar.get_schedule),
-    path(r"calendar/trainings", calendar.get_personal_schedule),
+    path(r"sports/<negint:sport_id>/schedule", calendar.get_schedule),
+    path(r"student/schedule", calendar.get_personal_schedule),
 
-    # reference management
-    path(r"reference/upload", reference.reference_upload),
+    # references
+    path(r"references/upload", reference.reference_upload),
 
     # self sport report
     path(r"selfsport/upload", self_sport_report.self_sport_upload),
     path(r"selfsport/types", self_sport_report.get_self_sport_types),
     path(r"selfsport/strava_parsing", self_sport_report.get_strava_activity_info),
 
-    # fitness test
-    path(r"fitnesstest/result", fitness_test.get_result),
-    path(r"fitnesstest/upload", fitness_test.post_student_exercises_result),
-    path(r"fitnesstest/upload/<int:session_id>", fitness_test.post_student_exercises_result),
-    path(r"fitnesstest/exercises", fitness_test.get_exercises),
-    path(r"fitnesstest/sessions", fitness_test.get_sessions),
-    path(r"fitnesstest/sessions/<int:session_id>", fitness_test.get_session_info),
-    path(r"fitnesstest/suggest_student", fitness_test.suggest_fitness_test_student),
+    # fitness tests
+    path(r"fitness-test/result", fitness_test.get_result),
+    path(r"fitness-test/upload", fitness_test.post_student_exercises_result),
+    path(r"fitness-test/upload/<int:session_id>", fitness_test.post_student_exercises_result),
+    path(r"fitness-test/exercises", fitness_test.get_exercises),
+    path(r"fitness-test/sessions", fitness_test.get_sessions),
+    path(r"fitness-test/sessions/<int:session_id>", fitness_test.get_session_info),
+    path(r"fitness-test/students/search", fitness_test.suggest_fitness_test_student),
 
-    # measurement
-    path(r"measurement/student_measurement", measurement.post_student_measurement),
-    path(r"measurement/get_results", measurement.get_results),
-    path(r"measurement/get_measurements", measurement.get_measurements),
+    # measurements
+    path(r"measurements/upload", measurement.post_student_measurement),
+    path(r"measurements/results", measurement.get_results),
+    path(r"measurements", measurement.get_measurements),
 
     path(r"semester", semester.get_semester),
 
@@ -96,7 +95,7 @@ urlpatterns = [
     path(r"analytics/attendance", analytics.attendance_analytics),
 
     # medical groups
-    path(r"medical_groups/", medical_groups.medical_groups_view),
+    path(r"medical-groups", medical_groups.medical_groups_view),
 ]
 
 urlpatterns.extend([
