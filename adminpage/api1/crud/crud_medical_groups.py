e793d00425152f663c8da7c32ab5d5ac76e715dd
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-07-26 22:19:36 +0300

    restore api1

--- adminpage/api1/crud/crud_medical_groups.py
+++
@@ -1,9 +0,0 @@
-from sport.models import MedicalGroup
-
-
-def get_medical_groups():
-    """
-    Return the list of medical groups
-    """
-    query = MedicalGroup.objects.all()
-    return list(query)
