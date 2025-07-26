2c7b3398dd14ba0df0cc77391f1fdc0c78422cb3
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-07-26 22:19:36 +0300

    restore api1

--- adminpage/api1/serializers/sport.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from sport.models import Sport
-
-
-class NewSportSerializer(serializers.ModelSerializer[Sport]):
-    class Meta:
-        model = Sport
-        fields = ('id', 'name', 'description')
